e838b853c9e426b71cf841341da0a80e18c35b95
Maple <maple.zhu7@gmail.com>
2022-08-22 10:02:17 -0700

    add car factory class

--- model/car.py
+++
@@ -1,9 +0,0 @@
-
-
-class Car:
-    def __init__(self):
-        self.engine_type
-        self.battery_type
-
-    def needs_service(self):
-        return self.engine_type.engine_should_be_serviced() or self.battery_type.battery_needs_service
